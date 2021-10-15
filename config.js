71f8dda0df28e0d200e7d10314cc011a17605926
khooii <89780367+khooii@users.noreply.github.com>
2021-10-15 21:59:38 +0700

    tang-anhthuuu

--- config.js
+++ config.js
@@ -1,23 +1,5 @@
-const CONFIG = {https://www.facebook.com/profile.php?id=100073533376934 
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Athuuu oii',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho khoi iu dấu <3',
-    reply: 'Yêu thì yêu khôi mà không yêu thì yêu khooi <33333333',
-    mess: 'Khooi biết mà 🥰. Athuu yeu khooi nhiều nhiều 😘😘',
-    messDesc: 'Tối công chúa của mình ngủ ngon nka :333.',
-    btnAccept: 'Dzaaaaaa bb cũng d nka <3','}
 
-                const CONFIG = {
+               const CONFIG = {
     titleWeb: "Tiêu đề web",
     introTitle: 'Athuu oiii',
     introDesc: `Trái đất vốn lạ thường
